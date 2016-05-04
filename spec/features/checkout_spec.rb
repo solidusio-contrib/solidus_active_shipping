4c3ea71c829e13aa976c4f4d05982458c77e0b68
Kevin Attfield <kevin@stembolt.com>
2016-05-04 15:09:58 -0700

    Add checkout feature spec
    It's hard to be much more sure than hitting the live UPS api and getting
    a real shipping rate. Although we should probably stub this is some way
    so that we're not
    
    A) Using actual live creds in this repo
    B) Tied to pricing whims of shipping providers

---
+++ spec/features/checkout_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe "Checkout", type: :feature do
+  include_context 'checkout setup'
+  include CheckoutHelper
+
+  include_context 'UPS setup'
+
+  context "with valid shipping address" do
+    let!(:valid_address) { create :address,
+      firstname: 'John',
+      lastname: 'Doe',
+      company: 'Company',
+      address1: '4157 Lawnview Ave',
+      city: 'Dallas',
+      state: create(:state, state_code: 'TX'),
+      zipcode: '75227',
+      phone: "(555) 555-5555"
+    }
+
+    it "does not break the per-item shipping method calculator", js: true do
+      add_to_cart(mug)
+
+      click_button "Checkout"
+
+      fill_in "order_email", with: "test@example.com"
+      click_button "Continue"
+      fill_in_address(valid_address)
+      click_button "Save and Continue"
+
+      # select default shipping
+      click_button "Save and Continue"
+      expect(page).to have_content("Shipping total: $68.81")
+    end
+  end
+end
