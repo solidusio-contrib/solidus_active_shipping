3b0cea6e6ac8eecf0f58ec91e540faad2918e1f8
Kevin Attfield <kevin@stembolt.com>
2016-05-04 14:55:59 -0700

    Add checkout setup
    There is a similar block of code in Solidus core, but we want to be very
    specific about the stock location and test product being created so that
    we can make assertions about shipping costs.

---
+++ spec/support/shared_contexts/checkout_setup.rb
@@ -0,0 +1,13 @@
+shared_context 'checkout setup' do
+  let!(:store) { create(:store) }
+  let!(:stock_location) { create :stock_location,
+    address1: '1600 Pennsylvania Ave NW',
+    city: 'Washington',
+    zipcode: '20500',
+    state: create(:state, state_code: 'DC')
+  }
+  let!(:shipping_method) { create(:shipping_method, calculator: Spree::Calculator::Shipping::Ups::NextDayAir.new) }
+  let!(:payment_method) { create(:check_payment_method) }
+  let!(:zone) { create(:zone) }
+  let!(:mug) { create(:product, name: 'RoR Mug', weight: 2) }
+end
