52199e596b9988138b4be15cf724e685822c78af
Frédéric Boivin <fboivin@Frederics-MacBook-Pro.local>
2016-05-09 16:06:46 -0400

    Tweak the bogus classes to they can used without stubbing everything

--- spec/lib/spree/active_shipping/bogus_carrier.rb
+++ spec/lib/spree/active_shipping/bogus_carrier.rb
@@ -3,12 +3,15 @@ module Spree
     # Bogus carrier useful for testing.  For some reasons the plugin version of this class does not work
     # properly (it fails to return a RateResponse)
     class BogusCarrier < ::ActiveShipping::Carrier
+      include RSpec::Mocks::ExampleMethods
+
       def name
         "BogusCarrier"
       end
 
+      # We're stubbing the rates because actual RateEstimate are too elaborate
       def find_rates(origin, destination, packages, options = {})
-        RateResponse.new(true, "Bogus response")
+        ::ActiveShipping::RateResponse.new(true, "success!", {}, :rates => [instance_double('rate', service_name: 'Bogus Calculator', price: 999)], :xml => "")
       end
     end
   end
--- spec/lib/spree/calculator/shipping/bogus_calculator.rb
+++ spec/lib/spree/calculator/shipping/bogus_calculator.rb
@@ -1,11 +1,9 @@
 module Spree
   module Calculator::Shipping
     module ActiveShipping
-      # Bogus calculator for testing purposes.  Uses the common functionality of ActiveShippingCalcualtor but
-      # doesn't actually use a real carrier to obtain rates.
       class BogusCalculator < Spree::Calculator::Shipping::ActiveShipping::Base
         def carrier
-          Spree::ActiveShipping::BogusCarrier
+          Spree::ActiveShipping::BogusCarrier.new
         end
 
         def self.description
