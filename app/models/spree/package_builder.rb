05220c53b682a5cc836adfb7c9a50aba0c15cd9d
Frédéric Boivin <fboivin63@gmail.com>
2016-06-02 10:56:07 -0700

    Distinguish Solidus packages from ActiveShipping packages
    Rename variables so it's easier to understand what is coming from
    Solidus and what is returned by ActiveShipping

--- app/models/spree/package_builder.rb
+++ app/models/spree/package_builder.rb
@@ -11,11 +11,11 @@ module Spree
       @shipping_calculator = shipping_calculator
     end
 
-    def process(package)
+    def process(solidus_package)
       # We compute and set the max_weight once, at the beginning of the process
-      @max_weight = get_max_weight(package)
+      @max_weight = get_max_weight(solidus_package)
 
-      packages(package)
+      packages(solidus_package)
     end
 
     # Configuration 
@@ -37,8 +37,8 @@ module Spree
 
     private
 
-    def convert_package_to_weights_array(package)
-      weights = package.contents.map do |content_item|
+    def convert_package_to_weights_array(solidus_package)
+      weights = solidus_package.contents.map do |content_item|
         item_weight = content_item.weight
         item_weight = default_weight if item_weight <= 0
         item_weight *= multiplier
@@ -61,10 +61,10 @@ module Spree
       []
     end
 
-    def convert_package_to_item_packages_array(package)
+    def convert_package_to_item_packages_array(solidus_package)
       packages = []
 
-      package.contents.each do |content_item|
+      solidus_package.contents.each do |content_item|
         variant  = content_item.variant
         quantity = content_item.quantity
         product  = variant.product
@@ -84,36 +84,36 @@ module Spree
     end
 
     # Generates an array of Package objects based on the quantities and weights of the variants in the line items
-    def packages(package)
-      packages = []
-      weights = convert_package_to_weights_array(package)
-      dimensions = convert_package_to_dimensions_array(package)
-      item_specific_packages = convert_package_to_item_packages_array(package)
+    def packages(solidus_package)
+      active_shipping_packages = []
+      weights = convert_package_to_weights_array(solidus_package)
+      dimensions = convert_package_to_dimensions_array(solidus_package)
+      item_specific_packages = convert_package_to_item_packages_array(solidus_package)
 
       if max_weight <= 0
-        packages << ::ActiveShipping::Package.new(weights.sum, dimensions, units: units)
+        active_shipping_packages << ::ActiveShipping::Package.new(weights.sum, dimensions, units: units)
       else
         package_weight = 0
         weights.each do |content_weight|
           if package_weight + content_weight <= max_weight
             package_weight += content_weight
           else
-            packages << ::ActiveShipping::Package.new(package_weight, dimensions, units: units)
+            active_shipping_packages << ::ActiveShipping::Package.new(package_weight, dimensions, units: units)
             package_weight = content_weight
           end
         end
-        packages << ::ActiveShipping::Package.new(package_weight, dimensions, units: units) if package_weight > 0
+        active_shipping_packages << ::ActiveShipping::Package.new(package_weight, dimensions, units: units) if package_weight > 0
       end
 
-      item_specific_packages.each do |package|
-        packages << ::ActiveShipping::Package.new(package.weight * multiplier, [package.length, package.width, package.height], units: units)
+      item_specific_packages.each do |product_package|
+        active_shipping_packages << ::ActiveShipping::Package.new(product_package.weight * multiplier, [product_package.length, product_package.width, product_package.height], units: units)
       end
 
-      packages
+      active_shipping_packages
     end
 
-    def get_max_weight(package)
-      order = package.order
+    def get_max_weight(solidus_package)
+      order = solidus_package.order
       order_ship_address_country = order.ship_address.country
 
       # Default value from calculator
