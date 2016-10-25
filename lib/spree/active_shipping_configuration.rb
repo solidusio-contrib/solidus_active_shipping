ded8749dbb328359edde4bfcf69d0e6d2e2ae57f
Frédéric Boivin <fboivin63@gmail.com>
2016-10-24 20:28:57 -0700

    Add CanadaPostPWS configuration
    This commits adds the needed settings for CanadaPostPWS.

--- lib/spree/active_shipping_configuration.rb
+++ lib/spree/active_shipping_configuration.rb
@@ -14,6 +14,13 @@ class Spree::ActiveShippingConfiguration < Spree::Preferences::Configuration
 
   preference :canada_post_login, :string, :default => "canada_post_login"
 
+  # The default values correspond to the official test credentials
+  # Source : https://www.canadapost.ca/cpo/mc/business/productsservices/developers/services/fundamentals.jsf
+  preference :canada_post_pws_userid, :string, :default => "6e93d53968881714"
+  preference :canada_post_pws_password, :string, :default => "0bfa9fcb9853d1f51ee57a"
+  preference :canada_post_pws_customer_number, :string, :default => "2004381"
+  preference :canada_post_pws_contract_number, :string, :default => "42708517"
+
   preference :units, :string, :default => "imperial"
   preference :unit_multiplier, :decimal, :default => 16 # 16 oz./lb - assumes variant weights are in lbs
   preference :default_weight, :integer, :default => 0 # 16 oz./lb - assumes variant weights are in lbs
